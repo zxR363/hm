6c39ded093621a1ecf8dd95c5a1f7b93e98fb27e
zxR363 <92975817+zxR363@users.noreply.github.com>
2021-11-09 22:47:02 +0300

    aaa

---
+++ js/exam2.js
@@ -0,0 +1,25 @@
+/*
+*   EXAM 2 PART 2
+*   Create a review page for a product or
+*   restaurant. A review should have a name,
+*   details (text), and a rating out of 5.
+*
+*   Requirements:
+*     -Can add a review
+*     -After adding a review, form should 'reset'
+*     -Review count with total number of reviews
+*     -Should have at least one image, can link
+*       to an image you found on google.
+*     -At least 10 styles applied. These can
+*       be applied in the header, (<style>)
+*       via DOM manipulation, or an external
+*       css file.
+*
+*   Please upload your HTML and JS files with your
+*   submission along with your css file if you used one.
+*/
+
+
+// YOUR CODE STARTS HERE
+
+// YOUR CODE ENDS HERE
