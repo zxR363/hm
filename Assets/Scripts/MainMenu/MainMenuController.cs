fe7e4520bc0f9af090f2493dde59d2353d8f3e58
Yusuf ÜSTÜNTEPE <yustuntepe@havelsan.com.tr>
2025-09-19 17:35:38 +0300

    Scenes added

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("BuildingSelectionScene");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Ayarlar açıldı.");
+        // Ayarlar panelini aktif et
+    }
+
+    public void OpenCharacterCreator()
+    {
+        SceneManager.LoadScene("CharacterCreationScene");
+    }
+}
