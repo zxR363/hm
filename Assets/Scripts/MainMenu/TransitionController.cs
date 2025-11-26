a5949eaa4235aed750f29bef3537ab36d13b5c9e
zxR363 <yazilimkasabasi@gmail.com>
2025-11-26 14:23:29 +0300

    transition Image'ler ekleniyor

--- Assets/Scripts/MainMenu/TransitionController.cs
+++ Assets/Scripts/MainMenu/TransitionController.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class TransitionController : MonoBehaviour
 {
+
+    [SerializeField] private Image transitionImage;
+    
+    Sprite randomSprite;
+    
     void Start()
     {
         StartCoroutine(LoadNextScene());
@@ -11,8 +18,26 @@ public class TransitionController : MonoBehaviour
 
     IEnumerator LoadNextScene()
     {
+        // 1️⃣ Rastgele görsel seç
+        transitionImage.sprite = GetRandomTransitionSprite();
+
         yield return new WaitForSeconds(3f); // Geçiş süresi
 
         SceneManager.LoadScene(SceneLoader.nextSceneName);
+    }
+
+
+    //RASTGELE RESIM SECIYOR
+    private static Sprite GetRandomTransitionSprite()
+    {
+        Sprite[] sprites = Resources.LoadAll<Sprite>("Images/TransitionImages");
+        if (sprites == null || sprites.Length == 0)
+        {
+            Debug.LogWarning("No transition images found in Resources/Images/TransitionImages");
+            return null;
+        }
+
+        int index = Random.Range(0, sprites.Length);
+        return sprites[index];
     }
 }
